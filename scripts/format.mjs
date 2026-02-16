00abaac600cd975ad6ce723ea4cf26f9470e8a78
Benjamin Møller Jensen <bemoje@bemoje.net>
2026-02-16 16:39:03 +0100

    fix: validate arguments for formatting script and refactor file collection logic

--- scripts/format.mjs
+++ scripts/format.mjs
@@ -1,29 +1,41 @@
 import cp from 'child_process'
+import fs from 'fs'
 
-function exec(cmd) {
-  return cp.execSync(cmd, { encoding: 'utf8' }).trim()
+const args = process.argv.slice(2)
+
+if (!args.some((arg) => ['--check', '--write', '-c', '-w'].includes(arg))) {
+  console.error(
+    `Error: You must provide either --check or --write (or their short versions -c or -w) as an argument.`,
+  )
+  process.exit(1)
 }
 
-const files = [] //
-  .concat(
-    exec(`git diff --cached --name-only`)
-      .split('\n')
-      .filter((f) => f.trim() !== ''),
-  )
-  .concat(
-    exec(`git diff --name-only`)
-      .split('\n')
-      .filter((f) => f.trim() !== ''),
-  )
-  .concat(
-    exec(`git ls-files --others --exclude-standard`)
-      .split('\n')
-      .filter((f) => f.trim() !== ''),
-  )
-if (files.length === 0) {
+async function exec(cmd) {
+  return cp
+    .execSync(cmd, { encoding: 'utf8' })
+    .trim()
+    .split('\n')
+    .map((f) => f.trim())
+    .filter(Boolean)
+}
+
+const commands = [
+  `git diff --cached --name-only`, //
+  `git diff --name-only`,
+  `git ls-files --others --exclude-standard`,
+]
+
+const files = Array.from(new Set((await Promise.all(commands.map(exec))).flat()))
+const existentFiles = files.filter((file) => {
+  return fs.existsSync(file)
+})
+
+if (existentFiles.length === 0) {
   console.log('No files to format.')
   process.exit(0)
 }
-const prettierCmd = `yarn prettier --ignore-unknown --write ${files.map((f) => `${f}`).join(' ')}`
-console.log(`Running: ${prettierCmd}`)
+
+const prettierCmd = `yarn prettier ${args.join(' ')} ${existentFiles.join(' ')}`
+
+console.debug(`Running: ${prettierCmd}`)
 cp.execSync(prettierCmd, { stdio: 'inherit' })
