661c15ac2735987ee20a92f8ddd7abd2ed5afef7
Benjamin Møller Jensen <bemoje@bemoje.net>
2026-02-19 14:55:25 +0100

    @mono/node: add createLogger, refactor timer with Logger support
    - Add createLogger with colored, structured log methods (start, done, info, log, warn, error, debug)
    - Rewrite timer to accept [name, description] tuple, pass Logger to task callback, and use humanize-duration for output
    - Export createLogger and Logger type from @mono/node
    - Add humanize-duration and @types/humanize-duration dependencies
    - Rewrite timer and createLogger tests
    - Adjust precommit script ordering to run test-coverage before parallel checks

---
+++ libs/node/src/createLogger.ts
@@ -0,0 +1,49 @@
+import colors from 'ansi-colors'
+import { isPrimitive } from 'es-toolkit/predicate'
+import { inspect } from 'node:util'
+
+export interface Logger {
+  start: (...args: unknown[]) => void
+  done: (...args: unknown[]) => void
+  info: (...args: unknown[]) => void
+  log: (...args: unknown[]) => void
+  warn: (...args: unknown[]) => void
+  error: (...args: unknown[]) => void
+  debug: (...args: unknown[]) => void
+}
+
+/**
+ * Creates a logger instance with colored output and consistent formatting.
+ */
+export function createLogger(name: string): Logger {
+  const NAME = name ? colors.dim.cyan(name) : name
+
+  const START = [NAME, colors.dim.gray('[START]')].filter(Boolean)
+  const DEBUG = [NAME, colors.dim.magenta('[DEBUG]')].filter(Boolean)
+  const INFO = [NAME, colors.dim.gray('[INFO]')].filter(Boolean)
+  const DONE = [NAME, colors.dim.green('[DONE] ')].filter(Boolean)
+  const WARN = [NAME, colors.dim.yellow('[WARN] ')].filter(Boolean)
+  const ERROR = [NAME, colors.dim.red('[ERROR]')].filter(Boolean)
+
+  const grayArgs = createColoredArgs(colors.gray)
+  const cyanArgs = createColoredArgs(colors.cyan)
+  const yellowArgs = createColoredArgs(colors.yellow)
+  const redArgs = createColoredArgs(colors.red)
+
+  return {
+    start: (...args: unknown[]) => console.info(...START, ...args),
+    done: (...args: unknown[]) => console.info(...DONE, ...args),
+    info: (...args: unknown[]) => console.info(...INFO, ...grayArgs(args)),
+    log: (...args: unknown[]) => console.log(...(NAME ? [NAME, ...args] : args)),
+    warn: (...args: unknown[]) => console.warn(...WARN, ...yellowArgs(args)),
+    debug: (...args: unknown[]) => console.debug(...DEBUG, ...cyanArgs(args)),
+    error: (...args: unknown[]) =>
+      console.error(inspect([NAME, ERROR, ...redArgs(args)]), { depth: null, colors: true }),
+  }
+}
+
+function createColoredArgs(colorFn: (str: string) => string) {
+  return (args: unknown[]) => {
+    return args.map((arg) => (isPrimitive(arg) ? colorFn(String(arg)) : arg))
+  }
+}
--- libs/node/src/timer.ts
+++ libs/node/src/timer.ts
@@ -1,23 +1,37 @@
-import { round } from '@mono/number'
-import { Any } from '@mono/types'
 import { isPromise } from 'node:util/types'
+import { createLogger, type Logger } from './createLogger'
+import colors from 'ansi-colors'
+import humanizeDuration from 'humanize-duration'
 
 /**
- * Executes a task and logs the execution time with the specified name.
+ * Executes a task and logs the execution time.
  */
-export function timer(name: string, task: (...args: any[]) => Any | Promise<Any>) {
+export function timer<T>(
+  arg: string | [name: string, description: string],
+  task: (log: Logger, name: string) => T,
+): T {
   const t0 = process.hrtime.bigint()
-  const printResult = () =>
-    console.debug({ timer: name, ms: round(Number(process.hrtime.bigint() - t0) / 1000000, 3) })
-  const rv = task()
+  const [name, description] = Array.isArray(arg) ? arg : [arg, '']
+  const log = createLogger(name)
 
-  if (!isPromise(rv)) {
-    printResult()
-    return rv
+  if (name) {
+    log.start(description)
   }
 
-  return rv.then((retval) => {
-    printResult()
+  const retval = task(log, name)
+
+  if (!isPromise(retval)) {
+    return done(retval)
+  }
+
+  return (retval as Promise<Awaited<T>>).then((result) => {
+    return done(result)
+  }) as T
+
+  function done(retval: T) {
+    const ns = process.hrtime.bigint() - t0
+    const ms = Math.floor(Number(ns) / 1000000)
+    log.done(colors.dim(humanizeDuration(ms)))
     return retval
-  })
+  }
 }
