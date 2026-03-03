f8a33b7db4cc30469b7d9f53697829bb1b4e271e
Benjamin Møller Jensen <bemoje@bemoje.net>
2026-03-03 20:57:29 +0100

    fix(types): add eslint-disable for any in StrictEquals
    Suppress lint error for `any` in generic distribution check in `DeepEquals.ts`.

--- libs/types/src/DeepEquals.ts
+++ libs/types/src/DeepEquals.ts
@@ -56,8 +56,9 @@ export type IsStrictEqual<T, Target> = StrictEquals<T, Target> extends never ? f
  * Test equality with strict matching strategty.
  * @returns {<T>|never }
  */
- 
+
 export type StrictEquals<T, Target> =
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   T extends any ?
     IsDeepEqual<T, Target> extends true ?
       T
