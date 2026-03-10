497fe45c6869b2387e764c2ce0991e1e8fd73501
Benjamin Møller Jensen <bemoje@bemoje.net>
2026-03-10 01:17:47 +0100

    feat(types): add AllKeys and CommonKeys type helpers

---
+++ libs/types/src/CommonKeys.ts
@@ -0,0 +1,19 @@
+import type { RequiredKeysOf } from 'type-fest'
+import type { SharedUnionFields } from 'type-fest'
+
+/**
+ * Type helper to get the common keys of an array of objects. If there are no common keys, it returns `string[]` to allow for any keys.
+ * @template T - The type of values in the input objects.
+ * @returns An array of keys that are present in all input objects, or `string[]` if there are no common keys.
+ * @example ```ts
+ * const objects = [
+ *   { a: 1, b: 2, d: 4 },
+ *   { a: 1, b: 2, c: 3 },
+ * ];
+ * type CommonKeysOfObjects = CommonKeys<typeof objects>;
+ * //=> "a" | "b"
+ * ```
+ */
+export type CommonKeys<T extends object[]> =
+  RequiredKeysOf<SharedUnionFields<T[number]> & object>[] extends never[] ? string[]
+  : RequiredKeysOf<SharedUnionFields<T[number]> & object>[]
